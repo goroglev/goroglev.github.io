6e1026af0c1359c7a57d835d2bd6cb56e227c053
goroglev <tarajos.goethe@gmail.com>
2021-04-16 18:15:17 +0200

    computing and displaying the season for each concert

---
+++ js/misc.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+function concertSeason(concertDateString) {
+	var concertDate = moment(concertDateString, 'DD.MM.YYYY', 'de')
+	var season = (concertDate.year()-1) + '/' + (concertDate.year()%100);
+	if (concertDate.month() > 7) { // Months start from 0 in js, so 7 is August
+								   // New seasons start in September.
+  		season = (concertDate.year()) + '/' + (concertDate.year()%100+1);
+	}
+	return season;
+}
