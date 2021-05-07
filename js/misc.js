1d52d4a90c84decf3ae5dc1198a119d3a123d29d
goroglev <tarajos.goethe@gmail.com>
2021-05-07 15:06:56 +0200

    styling concerts, adding seasons

--- js/misc.js
+++ js/misc.js
@@ -2,10 +2,10 @@
 
 function concertSeason(concertDateString) {
 	var concertDate = moment(concertDateString, 'DD.MM.YYYY', 'de')
-	var season = (concertDate.year()-1) + '/' + (concertDate.year()%100);
+	var season = (concertDate.year()-1) + ' / ' + (concertDate.year()%100);
 	if (concertDate.month() > 7) { // Months start from 0 in js, so 7 is August
 								   // New seasons start in September.
-  		season = (concertDate.year()) + '/' + (concertDate.year()%100+1);
+  		season = (concertDate.year()) + ' / ' + (concertDate.year()%100+1);
 	}
 	return season;
 }
